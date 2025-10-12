d592544b512f11fc89fa0a09bd50d65328baf5b7
isra-osvaldo <israel.acuna7@gmail.com>
2025-10-12 18:50:38 -0300

    Add removeFromCart functionality to Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import { useMemo } from "react"
 
-export default function Header({cart}) {
+export default function Header({cart, removeFromCart}) {
 
   // State Derivado 
   const isEmpty = useMemo( () => cart.length === 0, [cart])  
@@ -64,7 +64,11 @@ export default function Header({cart}) {
                               </button>
                             </td>
                             <td>
-                              <button className="btn btn-danger" type="button">
+                              <button 
+                                className="btn btn-danger" 
+                                type="button"
+                                onClick={() => removeFromCart(guitar.id)}
+                              >
                                 X
                               </button>
                             </td>
